eceb1259b8728c1edce7d0086866f73c8839314e
Your GitHub Username <Your GitHub Email>
2025-04-12 10:59:28 +0500

    changes done

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,8 @@
 import { Spotlight } from "../components/ui/Spotlight";
-import './ui/HeroSection.css';
+import "./ui/HeroSection.css";
 import { Button } from "../components/ui/moving-border";
 import React from "react";
+import Link from "next/link";
 
 const HeroSection: React.FC = () => {
   return (
@@ -11,7 +12,10 @@ const HeroSection: React.FC = () => {
         justify-center relative overflow-hidden
         mx-auto py-10 md:py-0 bg-grid-white/[0.02] bg-black/[0.96]"
     >
-      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
 
       <div className="p-4 relative z-10 w-full text-center top-14">
         <h1 className="mt-20 md:mt-0 text-8xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
@@ -19,11 +23,13 @@ const HeroSection: React.FC = () => {
         </h1>
         <p className="flex space-x-8 mt-10 font-normal text-2xl md:text-lg text-neutral-300 max-w-lg mx-auto top-80 para">
           Dive into our comprehensive music courses and transform your musical
-          journey today. Whether you&apos;re a beginner or looking to refine your
-          skills, join us to unlock your true potential.
+          journey today. Whether you&apos;re a beginner or looking to refine
+          your skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Button href="/courses">Explore Courses</Button>
+          <Link href="/courses">
+            <Button>Explore Courses</Button>
+          </Link>
         </div>
       </div>
     </div>
