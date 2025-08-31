d91ee4e66ed49fcec67fb59f6d2ede7b9a5d95dd
Your GitHub Username <Your GitHub Email>
2025-09-01 00:07:43 +0500

    Update HeroSectiion,Spotlight and nextConfigFile

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'], // Add Unsplash domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
+  allowedDevOrigins: [
+    'http://192.168.0.104:3000', // Replace with your dev server address and port
+  ],
 };
 
 module.exports = nextConfig;
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,26 +7,26 @@ import Link from "next/link";
 const HeroSection: React.FC = () => {
   return (
     <div
-      className="h-auto md:h-[67rem] w-full 
+      className="h-[700px] md:h-[800px] lg:h-[1000px]  w-ful  
         rounded-md flex flex-col items-center
         justify-center relative overflow-hidden
-        mx-auto py-10 md:py-0 bg-grid-white/[0.02] bg-black/[0.96]"
+        mx-auto py-10  bg-grid-white/[0.02] bg-black/[0.96]"
     >
       <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
+        className="-top-40 sm:left-40 md:left-60 md:-top-20 "
         fill="white"
       />
 
       <div className="p-4 relative z-10 w-full text-center top-14">
-        <h1 className="mt-20 md:mt-0 text-8xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+        <h1 className="mt-20 md:mt-0 text-4xl sm:text-6xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master The Art Of Music
         </h1>
-        <p className="flex space-x-8 mt-10 font-normal text-2xl md:text-lg text-neutral-300 max-w-lg mx-auto top-80 para">
+        <p className="flex space-x-8 mt-10  font-normal text-sm sm:lg md:text-2xl text-neutral-300 max-w-lg mx-auto top-80 para">
           Dive into our comprehensive music courses and transform your musical
           journey today. Whether you&apos;re a beginner or looking to refine
           your skills, join us to unlock your true potential.
         </p>
-        <div className="mt-4">
+        <div className="mt-4 mb-10" >
           <Link href="/courses">
             <Button>Explore Courses</Button>
           </Link>
